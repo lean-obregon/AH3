2d56d17a89d8fc224877dc05046200d4474fe4bb
Lean Obregon <lean.obregon@fiserv.com>
2023-10-16 21:40:48 -0400

    recursion fizzbuzz now works

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,24 +1,8 @@
 import java.util.Scanner;
 
 public class fizzbuzz {
-    public static void num(int number) {
-        if (number%3 == 0 && number%5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(number%3 == 0) {
-                System.out.println("Fizz");
-            } else if (number%5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(number);
-            }
-    }
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int n = console.nextInt();
-        
-
-        for (int i = 1; i <= n; i++) {
+    public static void fizzbuzz(int number) {
+        for (int i = 1; i <= number; i++) {
             if (i%3 == 0 && i%5 == 0) {
                 System.out.println("FizzBuzz");
             } else if(i%3 == 0) {
@@ -29,31 +13,37 @@ public class fizzbuzz {
                 System.out.println(i);
             }
         }
+    }
 
+    public static void fizzbuzzRecursion(int currentNum, int number) {
+        if (currentNum > number) {
+            return;
+        }
+
+        if (currentNum%3 == 0 && currentNum%5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(currentNum%3 == 0) {
+                System.out.println("Fizz");
+            } else if (currentNum%5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(currentNum);
+            }
+        fizzbuzzRecursion(currentNum + 1, number);
+    }
+    
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = console.nextInt();
+        fizzbuzz(n);
+        
         System.out.println("Start of recursion");
         System.out.println("Enter a number: ");
         n = console.nextInt();
+        
+        fizzbuzzRecursion(1, n);
 
         console.close();
-        if (n >= 1) {
-            if (n%3 == 0 && n%5 == 0) {
-                System.out.println("FizzBuzz" + (n--));
-                // n--;
-                // return;
-            } else if(n%3 == 0) {
-                System.out.println("Fizz" + (n--));
-                // n--;
-                // return;
-            } else if (n%5 == 0) {
-                System.out.println("Buzz" + (n--));
-                // n--;
-                // return;
-            } else {
-                System.out.println(n + (n--));
-                // n--;
-                // return;
-            }
-        }
     }
 }
-
